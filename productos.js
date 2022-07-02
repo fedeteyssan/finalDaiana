e985046863ccf0067aecd218294c4f223bc49fbb
federico.teyssandier <fedeteyssan@gmail.com>
2022-07-02 12:11:47 -0300

    First commit

---
+++ productos.js
@@ -0,0 +1,55 @@
+var productos = [
+    {
+        id: 0,        
+        nombre: 'Print Japo #1',
+        precio: 850,
+        descripcion: 'Print realizado por Luciano Vazquez Lococo',
+        imagen: 'prints/1.png',
+        categoria: 'Prints Japo',
+    },
+
+    {
+        id: 1,        
+        nombre: 'Print Japo #2',
+        precio: 790,
+        descripcion: 'Print realizado por Luciano Vazquez Lococo',
+        imagen: 'prints/2.png',
+        categoria: 'Prints Japo',
+    },
+
+    {
+        id: 2,        
+        nombre: 'Print Japo #3',
+        precio: 920,
+        descripcion: 'Print realizado por Luciano Vazquez Lococo',
+        imagen: 'prints/5.png',
+        categoria: 'Prints Japo',
+    },
+
+    {
+        id: 3,        
+        nombre: 'Print Tradi #1',
+        precio: 570,
+        descripcion: 'Print realizado por Chipi Classics Tattoo',
+        imagen: 'prints/3.png',
+        categoria: 'Prints Tradi',
+    },
+
+    {
+        id: 4,        
+        nombre: 'Print Tradi #2',
+        precio: 640,
+        descripcion: 'Print realizado por Chipi Classics Tattoo',
+        imagen: 'prints/4.png',
+        categoria: 'Prints Tradi',
+    },
+
+    {
+        id: 5,        
+        nombre: 'Print Tradi #3',
+        precio: 660,
+        descripcion: 'Print realizado por Chipi Classics Tattoo',
+        imagen: 'prints/6.png',
+        categoria: 'Prints Tradi',
+    },   
+];
